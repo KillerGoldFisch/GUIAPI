c97b5571acd99f24857946c42afecca697c25e19
austinv11 <austinv11@gmail.com>
2014-06-15 12:51:42 -0400

    Added actual debug stuff

--- GUIAPI/src/main/java/io/github/austinv11/GUIAPI/GUIAPI.java
+++ GUIAPI/src/main/java/io/github/austinv11/GUIAPI/GUIAPI.java
@@ -1,14 +1,21 @@
 package io.github.austinv11.GUIAPI;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GUIAPI extends JavaPlugin{
+public class GUIAPI extends JavaPlugin implements Listener{
 	private static boolean debug = false;
+	private Server server = Bukkit.getServer();
 	@Override
     public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("GUIAPI v"+Menu.getAPIVersion()+" has been enabled. Remember, it does nothing on its own.");
     }
  
@@ -21,6 +28,20 @@ public class GUIAPI extends JavaPlugin{
     }
     public static void setDebugStatus(boolean isDebug){
     	debug = isDebug;
+    }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onMenuInteract(MenuInteractEvent event){
+    	if (debug){
+    		server.broadcastMessage("Menu"+event.getMenuName());
+    		server.broadcastMessage("Is Menu Open: "+event.getMenu().isMenuOpen());
+    		server.broadcastMessage("Button: "+event.getButtonName());
+    		server.broadcastMessage("Button Item: "+event.getButtonItem().getType().toString());
+    		server.broadcastMessage("Did Button Toggle: "+event.getMenu().isButtonToggled(event.getSlot()));
+    		server.broadcastMessage("Slot: "+event.getSlot());
+    		server.broadcastMessage("ClickType: "+event.getClickType().toString());
+    		server.broadcastMessage("Player: "+event.getPlayer().getDisplayName());
+    		server.broadcastMessage("Is Cancelled: "+event.isCancelled());
+    	}
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
